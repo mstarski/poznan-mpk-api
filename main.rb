c1b6181ebfb6165844e00f0fd4f5f02dbd188027
Michał Starski <michalstarski99@gmail.com>
2019-07-21 21:26:09 +0200

    initial path finding script ready

--- main.rb
+++ main.rb
@@ -4,8 +4,35 @@ require_relative('./traverse')
 require 'json'
 require 'pp'
 
-ztm_data = JSON.load(File.new('./ztm_data.json'))
-stops_data = ztm_data[:stops_data]
-working_line_numbers = ztm_data[:working_line_numbers]
+def ztm_tram_routes(start, stop)
+	ztm_data = JSON.load(File.new('./ztm_data.json'))
+	stops_data = ztm_data['stops_data']
+	working_line_numbers = ztm_data['working_line_numbers']
 
-pp working_line_numbers
+	line_number_nodes = {}
+	#Map working line numbers to graph nodes
+	working_line_numbers.each {|number|
+		line_number_nodes[number] = Node.new(number)
+	}
+
+	stop_nodes = {}
+	#Map stops to graph nodes
+	stops_data.each {|stop_code, data|
+		stop_node = Node.new(stop_code)
+		data['lines'].each {|line|
+			stop_node.add_edge(line_number_nodes[line])
+		}
+		stop_nodes[data['name']] = stop_node
+	}
+
+	graph = Graph.new
+	graph.root = stop_nodes[start]
+	if graph.root.nil?
+		p 'Start does not exist.'
+	end
+	route = bfs(graph, stop_nodes[stop].value)
+	pp route
+end
+
+#Example usage
+ztm_tram_routes('Szymanowskiego', 'Rondo Rataje')
