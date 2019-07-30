49ef7823489c78044b203ac3cfa7eddbacedbf81
Michał Starski <michalstarski99@gmail.com>
2019-07-30 21:32:10 +0200

    added sinatra and made app exposable to the internet

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org" do
+    gem 'nokogiri'
+    gem 'sinatra'
+    gem 'thin'
+end
+
+git_source(:github) {|repo_name| "https://github.com/michalStarski/poznan-mpk-api" }
--- main.rb
+++ main.rb
@@ -1,5 +1,16 @@
 require_relative('./scripts/find_routes.rb')
 require_relative('./scripts/timetable.rb')
+require 'sinatra'
+require 'json'
 
-#Example usage
-p Timetable::routes('Rondo Rataje', 'Os. Sobieskiego')
+get '/api' do
+    '<p>This is the poznan-mpk-api:</p> 
+    <a href="https://github.com/michalStarski/poznan-mpk-api">Github</a>'
+end
+
+post '/api/get_route' do
+    from = params['from']
+    to = params['to']
+    response.headers['Content-Type'] = 'application/json'
+    return Timetable::routes(from, to).to_json
+end
