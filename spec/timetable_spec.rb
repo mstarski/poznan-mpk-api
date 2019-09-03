2775c4f430a53c926b427f11fa24ceedd412a062
Michał Starski <michalstarski99@gmail.com>
2019-09-03 19:38:00 +0200

    Added tests for the .quick_look method in the Timetable module

--- spec/timetable_spec.rb
+++ spec/timetable_spec.rb
@@ -23,10 +23,10 @@ describe Timetable do
         expect(sample[0]).to include(:day, :minutes, :stop_name,
                                      :final_destination, :hour, :is_today, :journey_time, :dest, :line)
       end
-      it 'should have an arrival for today or later' do
+
+      it 'have got a valid time' do
         t = Time.now
         sample.each do |part|
-          p part
           expect(part[:day]).to be_between(0, 6)
           expect(part[:day]).to eq(0).or be >= t.wday
           expect(part[:minutes].to_i).to be_between(0, 60)
@@ -35,11 +35,6 @@ describe Timetable do
             expect(part[:is_today]).to be true
           else
             expect(part[:is_today]).to be false
-          end
-          if t.hour < part[:hour].to_i
-            expect(part[:minutes].to_i).to be < t.min
-          else
-            expect(part[:minutes].to_i).to be > t.min
           end
         end
       end
@@ -54,13 +49,49 @@ describe Timetable do
   end
 
   describe '.quick_look' do
-    it 'given wrong stop name' do
+    context 'given wrong stop name' do
+      output = Timetable.quick_look(12, 'dasdads')
+      it 'returns an empty array' do
+        expect(output).to be_empty
+      end
     end
-    it 'given wrong line number' do
+
+    context 'given wrong line number' do
+      output = Timetable.quick_look(111, 'os. piastowskie')
+      it 'returns an empty array' do
+        expect(output).to be_empty
+      end
     end
-    it 'given wrong stop name and line number' do
+
+    context 'given wrong stop name and line number' do
+      output = Timetable.quick_look(111, 'dasdasd')
+      it 'returns an empty array' do
+        expect(output).to be_empty
+      end
     end
-    it 'given valid stop and line number' do
+
+    context 'given valid stop and line number' do
+      output = Timetable.quick_look(12, 'os. piastowskie')
+      it 'has two elements one per direction' do
+        expect(output).to be_instance_of(Array)
+        expect(output.length).to eq(2)
+      end
+
+      it 'has all the defined keys as well as points to the correct stop and line' do
+        output.each do |entry|
+          expect(entry.keys).to include(:day, :minutes, :stop_name, :final_destination,
+                                        :hour, :is_today, :line)
+          expect(entry[:stop_name].downcase).to eq 'os. piastowskie'
+          expect(entry[:line].to_i).to eq 12
+        end
+      end
+    end
+
+    context 'given the stop is the last one on the route' do
+      output = Timetable.quick_look(16, 'os. sobieskiego')
+      it 'should have only one entry' do
+        expect(output.length).to eq 1
+      end
     end
   end
 end
